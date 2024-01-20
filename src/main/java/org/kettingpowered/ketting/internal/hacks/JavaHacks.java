0f9e155bb402187ef1d0b6130ac05ba0b134c323
C0D3 M4513R <28912031+C0D3-M4513R@users.noreply.github.com>
2024-01-20 18:22:01 +0100

    Add loadExternalFileSystems hack back

--- src/main/java/org/kettingpowered/ketting/internal/hacks/JavaHacks.java
+++ src/main/java/org/kettingpowered/ketting/internal/hacks/JavaHacks.java
@@ -1,5 +1,11 @@
 package org.kettingpowered.ketting.internal.hacks;
 
+import java.lang.reflect.Field;
+import java.net.URLClassLoader;
+import java.nio.file.spi.FileSystemProvider;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ServiceLoader;
 import java.util.Set;
 
 /**
@@ -7,6 +13,23 @@ import java.util.Set;
  */
 @SuppressWarnings("unused")
 public final class JavaHacks {
+    //Loads the union file system (and others) from the classpath
+    public static void loadExternalFileSystems(URLClassLoader loader) {
+        try {
+            ServerInitHelper.addOpens("java.base", "java.nio.file.spi", "ALL-UNNAMED");
+            List<String> knownSchemes = FileSystemProvider.installedProviders().stream().map(FileSystemProvider::getScheme).toList();
+            ServiceLoader<FileSystemProvider> sl = ServiceLoader.load(FileSystemProvider.class, loader);
+            List<FileSystemProvider> newProviders = sl.stream().map(ServiceLoader.Provider::get).filter(provider -> !knownSchemes.contains(provider.getScheme())).toList();
+
+            final Field installedProviders = FileSystemProvider.class.getDeclaredField("installedProviders");
+            installedProviders.setAccessible(true);
+            @SuppressWarnings("unchecked") List<FileSystemProvider> providers = new ArrayList<>((List<FileSystemProvider>) installedProviders.get(null));
+            providers.addAll(newProviders);
+            installedProviders.set(null, providers);
+        } catch (Exception e) {
+            throw new RuntimeException("Could not load new file systems", e);
+        }
+    }
     //Fixes weird behaviour with org.apache.commons.lang.enum where enum would be seen as a java keyword
     public static void clearReservedIdentifiers() {
         try {
